4041ce0aace0df476f47b587f81c2538b5943ca9
yuebu <297358842@qq.com>
2021-11-29 22:38:46 +0800

    debug , v0.2

--- DaQiaoRen-spider.py
+++ DaQiaoRen-spider.py
@@ -31,7 +31,7 @@ def htmlAnalysis_DaQiaoRenQusetionBank(htmlText):
     # tree = etree.parse(htmlPath, parser=parser)
     numberRegx = r'\d*'
     tree = etree.HTML(htmlText)
-    htmlAnalysis_question = tree.xpath("/html/body/div//h1/span/text()")
+    htmlAnalysis_question = tree.xpath("/html/body/div//h1/span//text()")
     index = 0
     try:
         index = re.findall(numberRegx, htmlAnalysis_question[0])[0]
@@ -45,44 +45,70 @@ def htmlAnalysis_DaQiaoRenQusetionBank(htmlText):
     try:
         htmlAnalysis_questionImg_new = htmlAnalysis_questionImg[0]
     except IndexError:
-        htmlAnalysis_questionImg_new = ''
+        htmlAnalysis_questionImg = tree.xpath("/html/body/div//h1/span//@src")
+        try:
+            htmlAnalysis_questionImg_new = htmlAnalysis_questionImg[0]
+        except IndexError:
+            htmlAnalysis_questionImg_new = ''
     # print('htmlAnalysis_questionImg:', htmlAnalysis_questionImg_new)
 
-    htmlAnalysis_options = tree.xpath("/html/body/div//ul[@id='ul_answers']/label/text()")
+    htmlAnalysis_options = tree.xpath("/html/body/div//ul[@id='ul_answers']/label//text()")
     htmlAnalysis_options_new = []
-    for op in htmlAnalysis_options:
-        if len(op.strip().replace(r'\r\n', '')) > 0:
-            htmlAnalysis_options_new.append(op)
-    # print('htmlAnalysis_options:', htmlAnalysis_options_new)
+    opStr = ''.join(htmlAnalysis_options)
+    htmlAnalysis_options_new = re.sub(globalVar.regxOptions_group, '\\n', opStr, 0, re.MULTILINE)
+    htmlAnalysis_options_new = re.findall(r'.+', htmlAnalysis_options_new)
+    # for op in htmlAnalysis_options:
+    #     if len(op.strip().replace(r'\r\n', '')) > 0:
+    #         htmlAnalysis_options_new.append(op)
+    # print('htmlAnalysis_options_new:', htmlAnalysis_options_new)
+    # print('type:', type(htmlAnalysis_options_new))
+    # for index, tem in htmlAnalysis_options_new:
+    #     print('index:', index, '。tem:', tem)
 
-    htmlAnalysis_answer = tree.xpath("/html/body/div//div[@class='daanjiexi']/p/text()")
+    htmlAnalysis_answer = tree.xpath("/html/body/div//div[@class='daanjiexi']/p//text()")
     htmlAnalysis_answer_new = []
     try:
         htmlAnalysis_answer_new.append(htmlAnalysis_answer[0])
     except IndexError:
-        pass
+        # 答案解析中只有图片时，无法识别答案
+        # print('需要手动在answer中标注答案')
+        htmlAnalysis_answer_new = ['', '']
     else:
         try:
             htmlAnalysis_answer_new.append(''.join(htmlAnalysis_answer[1:]))
         except IndexError:
             pass
     # print('htmlAnalysis_answer:', htmlAnalysis_answer_new)
+    htmlAnalysis_answerImg = tree.xpath("/html/body/div//div[@class='daanjiexi']/p/img/@scr")
+    try:
+        htmlAnalysis_answerImg_new = htmlAnalysis_answerImg[0]
+    except IndexError:
+        htmlAnalysis_answerImg_new = ''
 
-    htmlAnalysis_questionUrl = tree.xpath("/html/body/header//div[@class='div3']//a/@href")
+
+    # htmlAnalysis_questionUrl = tree.xpath("/html/body/header//div[@class='div3']//a/@href")
     # print('htmlAnalysis_questionUrl:', htmlAnalysis_questionUrl)
 
     resDic['htmlAnalysis_question'] = htmlAnalysis_question_new
     resDic['htmlAnalysis_questionImg'] = htmlAnalysis_questionImg_new
     resDic['htmlAnalysis_options'] = htmlAnalysis_options_new
     resDic['htmlAnalysis_answer'] = htmlAnalysis_answer_new
+    resDic['htmlAnalysis_answerImg'] = htmlAnalysis_answerImg_new
     print(resDic)
     return resDic
 
 
 def saveAsJson(path, name, index: int, question, questionImgUrl, options: list, answer: str, detail):
+    pass
+
+
+def getOptionsDic(questionType, name, index: int, question, questionImgUrl, options: list, answer: str, detail, detaiImglUrl):
     # answer=['contentA', 'contentB', 'contentC']
+    errorList = []
+    dic = {}
+    res = {'optionsDic': dic, 'error': errorList}
+
     answer = answer.upper()
-    global dic
     answerList = []
     optionsDic = {}
     regxName = globalVar.regxOptionsABCDE
@@ -93,6 +119,8 @@ def saveAsJson(path, name, index: int, question, questionImgUrl, options: list, 
             answerName = re.findall(regxName, option)[0].upper()
         except IndexError:
             print('选项错误')
+            error = f'option:{option}选项错误'
+            errorList.append(error)
         else:
             optionsDic['name'] = answerName
             try:
@@ -112,9 +140,14 @@ def saveAsJson(path, name, index: int, question, questionImgUrl, options: list, 
     if re.match(globalVar.regexIsUrl, questionImgUrl) is not None:
         status = download_img(questionImgUrl)
     # if status:
-    question = question + f'<img src="http://file.zmkj6.top/{os.path.basename(questionImgUrl)}">'
+        question = question + f'<img src="http://file.zmkj6.top/{os.path.basename(questionImgUrl)}">'
+
+    if re.match(globalVar.regexIsUrl, detaiImglUrl) is not None:
+        status = download_img(detaiImglUrl)
+    # if status:
+        detail = detail + f'<img src="http://file.zmkj6.top/{os.path.basename(detaiImglUrl)}">'
     dic = {
-        "type": 0,
+        "type": questionType,
         "id": 0,
         "name": name,
         "content": question,
@@ -122,9 +155,10 @@ def saveAsJson(path, name, index: int, question, questionImgUrl, options: list, 
         'detail': detail,
         'index': index
     }
-    print(dic)
-    json.dump(dic, open(path, 'a', encoding='utf-8', newline='\n'), indent=4, ensure_ascii=False)
-    return True
+    # print(dic)
+    # json.dump(dic, open(path, 'a', encoding='utf-8', newline='\n'), indent=4, ensure_ascii=False)
+    res = {'optionsDic': dic, 'error': errorList}
+    return res
 
 
 def download_img(img_url):
@@ -135,7 +169,7 @@ def download_img(img_url):
         'Referer': 'http: // www.daqiaoren.com / index.php?m = content & c = index & a = lists & catid = 124'
     }
     r = requests.get(img_url, headers=headers, stream=True)
-    print(r.status_code) # 返回状态码
+    # print(r.status_code) # 返回状态码
     if r.status_code == 200:
         open(f'.\data\picture\{os.path.basename(img_url)}', 'wb').write(r.content)  # 将内容写入图片
         print("done")
@@ -196,43 +230,62 @@ def url_zhishiPaper(headers, htmlAnalysis):
 
 
 def spider_DqQiaoRen(saveName, url):
+    pass
+
+
+def getQuestionDic(url):
     # with open('test.html', 'r', encoding='utf-8') as htmlFile:
     #     htmlText = htmlFile.read()
     # htmlPath = 'test.html'
     # htmlAnalysis_DaQiaoRenQusetionBank(htmlPath)
+    errorList = []
+    questionDic = {}
+    res = {'questionDic': questionDic, 'error': errorList}
     exam_url_A = url  # 'http://www.daqiaoren.com/index.php?m=content&c=index&a=show&catid=125&id=3409'
     temp1 = getHtml_DaQiaoRen(exam_url_A)
     questionDic = htmlAnalysis_DaQiaoRenQusetionBank(temp1)
 
-    questionType = ['单选题', '多选题']
-    path = f'./data/{saveName}.json'
+    # questionType = ['单选题', '多选题']
+    questionTypeSet = {'单选题': 0, '多选题': 1}
     name = ''
     index = questionDic['htmlAnalysis_question'][0]
     # img
     question = questionDic['htmlAnalysis_question'][1]
     questionImgUrl = questionDic['htmlAnalysis_questionImg']
-    for type in questionType:
-        if question.find(type) >= 0:
-            name = type
+    for typeEl in questionTypeSet:
+        if question.find(typeEl) >= 0:
+            name = typeEl
+            questionType = questionTypeSet[typeEl]
             break
     options = questionDic['htmlAnalysis_options']
     try:
         answer = re.findall(globalVar.regxAnswer, questionDic['htmlAnalysis_answer'][0])[0]
     except IndexError:
-        print('答案解析中有可能是图片')
-        return '错误'
+        answer = ''
+        errorList.append('答案解析中有可能是图片,需要手动在answer中标注答案')
+        print('答案解析中有可能是图片,需要手动在answer中标注答案')
+        # return '错误'
     detail = ''
     try:
         detail = questionDic['htmlAnalysis_answer'][1]
     except IndexError:
         pass
-    saveAsJson(path=path, name=name, index=index, question=question, questionImgUrl=questionImgUrl, options=options,
-               answer=answer, detail=detail)
+    questionDic_dic = getOptionsDic(questionType=questionType, name=name, index=index, question=question,
+                                questionImgUrl=questionImgUrl, options=options, answer=answer, detail=detail,
+                                detaiImglUrl=questionDic['htmlAnalysis_answerImg'])
+    questionDic = questionDic_dic['optionsDic']
+    # print(questionDic_dic)
+    if questionDic_dic['error']:
+        errorList.append('\\t'.join(questionDic_dic['error']))
     # dic['content'] = 'temp'
     # print(dic)
+    res = {'questionDic': questionDic, 'error': errorList}
+    return res
 
 
 if __name__ == '__main__':
+    errorList = []
+    res = {'error': errorList}
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
         'Cookie': 'PHPSESSID=2v9vgarlcr4v548jutpm7hoo71; BdXlz_auth=72fcx39rm9PWLzHuPkmVGGXyyIFBIyPeFRNuC439s2vjPwYE8HQI2qtc-444iWXMCcv6KQhBNK2o57kR_I-0ESMIhJJ_c0u1dOHvdDBBEXgVdOXbb_aHX5CYrljFXMabClgulns1g1tFgL4D5wVRA6WgmHe-; BdXlz__userid=4120wofNOdiQra-Jsysz2lIo8NzxKQo2PqiMfkBf9tYU; BdXlz__username=3d778wTBLkaquEzT7ClQc1cxtyPdj3CF0rxz5b-Yw6SydA; BdXlz__groupid=73cbgKhSXlxR-NHYVIWzx3lI4KHNfw3CWP29B21K; BdXlz__nickname=74d6B6ViHuGpBIiAXHgiQ3VKowhgMZ-hWeni2lF3G10gTTDRKBMSfKpVgfvXXC4A',
@@ -253,6 +306,29 @@ if __name__ == '__main__':
         # url_B0 = url_dic['url_B']
         url_noPage = re.findall(regxUrl, url_A0)[0]
         page_A0 = int(re.findall(regxPage, url_A0)[0])
+        path = f'./data/{saveName_A}.json'
+        res_question_dic_list = []
         for i in range(70):
             url_A_new = url_noPage + str(page_A0+i)
-            spider_DqQiaoRen(saveName_A, url_A_new)
+
+            questionDic_dic_includeError = getQuestionDic(url_A_new)
+            questionDic = questionDic_dic_includeError['questionDic']
+
+            if questionDic_dic_includeError['error']:
+                print(questionDic_dic_includeError)
+                error = saveName_A + f'第{i}题' + '\\t'.join(questionDic_dic_includeError['error'])
+                errorList.append(error)
+            # print(questionDic_dic)
+            # questionDic = questionDic_dic['questionDic']
+
+            # if questionDic_dic['error']:
+            #     error = saveName_A + f'第{i}题' + '\\t'.join(questionDic_dic['error'])
+            #     errorList.append(error)
+            res_question_dic_list.append(questionDic)
+        # 保存
+        json.dump(res_question_dic_list, open(path, 'a', encoding='utf-8', newline='\n'), indent=4, ensure_ascii=False)
+    with open('./log/log.txt', 'a') as fp:
+        # fp.writelines(errorList)
+        for temp in errorList:
+            fp.write(temp)
+            fp.write('\r\n')
--- globalVar.py
+++ globalVar.py
@@ -1,6 +1,7 @@
 import re
-regxOptionsABCDE = r'[\(\（][ \t]*([ABCDEabcde])[ \t]*[\)\）]'  # 查找题目选项
-regxOptionsABCDE_except = r'[\(\（][ \t]*[ABCDEabcde][ \t]*[\)\）]:*(.+)'  # 查找题目选项中的内容
+regxOptions_group = r"(?<!^)(?=(（|\()[ABCDEabcde](）|\)))"  # 如果str中含有（A***（B***时，分成[（A***,（B***]
+regxOptionsABCDE = r'[\(\（][ \t]*([ABCDEabcde])[ \t]*[\)\）]'  # 查找题目选项,例（A)*** 中提取出A
+regxOptionsABCDE_except = r'[\(\（][ \t]*[ABCDEabcde][ \t]*[\)\）]:*(.+)'  # 查找题目选项中的内容,例（A)*** 中提取出***
 regxAnswer = r'答[ \n]*案\W*(\w*)'
 regexIsUrl = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
@@ -8,4 +9,4 @@ regexIsUrl = re.compile(
         r'localhost|' #localhost...
         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
         r'(?::\d+)?' # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)  # 判断网址格式是否正确
